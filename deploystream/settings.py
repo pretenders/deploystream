2d61f06aba7484f5cad8e18c25d307d58622f334
Alex Couper <amcouper@gmail.com>
2013-03-03 21:51:36 +0000

    Add initial settings configuration for plugins.

--- deploystream/settings.py
+++ deploystream/settings.py
@@ -1,6 +1,17 @@
 
-SOURCE_CODE_PLUGINS = ['deploystream.providers.git_plugin.plugin.GitPlugin']
+SOURCE_CODE_PLUGINS = ['deploystream.providers.git_plugin.GitPlugin']
 PLANNING_PLUGINS = []
 BUILD_INFO_PLUGINS = []
 
 HIERARCHY_REGEXES = []
+
+
+PLUGIN_CONFIG = {
+    'GitPlugin': {
+        'code_dir': None
+    }
+}
+"""A dict of configuration information for the plugins.
+Keys are plugin class names, values are dictionaries to be passed in as kwargs
+during insantiation.
+"""
