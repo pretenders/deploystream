7a5ea099fce0069ce97d0a43486039ac3069ae51
Carles Barrobés <carles@barrobes.com>
2013-03-03 19:25:17 +0000

    Fix failing tests, and rename function

--- deploystream/providers/interfaces/__init__.py
+++ deploystream/providers/interfaces/__init__.py
@@ -1,15 +1,24 @@
 from zope import interface as zinterface
+from zope.interface import verify
 
 from build_info import IBuildInfoPlugin
 from planning import IPlanningPlugin
 from source_code_control import ISourceCodeControlPlugin
 
 
-def check_class_implements_interface(cls, interface):
-    "Check that the given class implements the given interface. Return Bool"
+def isimplementation(cls, interface):
+    """
+    Check that the given class implements the given interface.
+
+    Mimics the signature and behaviour of `isinstance`, for interfaces.
+
+    :returns:
+        A boolean
+    """
     zinterface.classImplements(cls, interface)
     try:
         zinterface.verify.verifyClass(interface, cls)
         return True
     except Exception:
+        #raise
         return False
--- tests/test_providers/test_interfaces.py
+++ tests/test_providers/test_interfaces.py
@@ -1,8 +1,9 @@
 from nose.tools import assert_true, assert_false
 
 from deploystream.providers.interfaces import (
-    check_class_implements_interface, IBuildInfoPlugin, IPlanningPlugin,
-    ISourceCodeControlPlugin)
+    isimplementation,
+    IBuildInfoPlugin, IPlanningPlugin, ISourceCodeControlPlugin,
+)
 
 
 class TestSourceCodeControlPluginInterface(object):
@@ -14,8 +15,7 @@ class TestSourceCodeControlPluginInterface(object):
             def set_merged_status(self, repo_name, hierarchy_tree):
                 pass
 
-        assert_true(check_class_implements_interface(
-                            MyPlugin, ISourceCodeControlPlugin))
+        assert_true(isimplementation(MyPlugin, ISourceCodeControlPlugin))
 
     def test_does_not_implement_source_control_plugin(self):
         class MyPlugin(object):
@@ -24,8 +24,7 @@ class TestSourceCodeControlPluginInterface(object):
             def set_merged_status(self, repo_name):
                 pass
 
-        assert_false(check_class_implements_interface(
-                            MyPlugin, ISourceCodeControlPlugin))
+        assert_false(isimplementation(MyPlugin, ISourceCodeControlPlugin))
 
 
 class TestBuildInfoPluginInterface(object):
@@ -34,14 +33,14 @@ class TestBuildInfoPluginInterface(object):
         class MyPlugin(object):
             def get_build_information(self, repo, branch, commit):
                 pass
-        assert_true(check_class_implements_interface(
-                            MyPlugin, IBuildInfoPlugin))
+
+        assert_true(isimplementation(MyPlugin, IBuildInfoPlugin))
 
     def test_does_not_implement_build_info_plugin(self):
         class MyPlugin(object):
             pass
-        assert_false(check_class_implements_interface(
-                            MyPlugin, IBuildInfoPlugin))
+
+        assert_false(isimplementation(MyPlugin, IBuildInfoPlugin))
 
 
 class TestPlanningPluginInterface(object):
@@ -50,11 +49,11 @@ class TestPlanningPluginInterface(object):
         class MyPlugin(object):
             def get_feature_info(self, feature_id):
                 pass
-        assert_true(check_class_implements_interface(
-                            MyPlugin, IPlanningPlugin))
+
+        assert_true(isimplementation(MyPlugin, IPlanningPlugin))
 
     def test_does_not_implement_planning_plugin(self):
         class MyPlugin(object):
             pass
-        assert_false(check_class_implements_interface(
-                            MyPlugin, IPlanningPlugin))
+
+        assert_false(isimplementation(MyPlugin, IPlanningPlugin))
