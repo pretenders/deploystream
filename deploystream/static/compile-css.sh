2d66680f984789d89ff0152ebf04cef65f000579
Carles Barrobés <carles@barrobes.com>
2013-03-24 21:02:11 +0000

    CSS files no longer sources

--- deploystream/static/compile-css.sh
+++ deploystream/static/compile-css.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Statically compile project LESS files as CSS
 
+mkdir -p css
 recess less/site.less --compile > css/site.css
 recess less/site.less --compile --compress > css/site.min.css
 recess less/responsive.less --compile > css/responsive.css
