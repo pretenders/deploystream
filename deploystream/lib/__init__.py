8b710262a7849ea532a630fb0a88eb91450b2c94
Alex Couper <amcouper@gmail.com>
2013-04-11 10:25:10 +0100

    Add comment as to why we do the cacerts copy.

--- deploystream/lib/__init__.py
+++ deploystream/lib/__init__.py
@@ -7,7 +7,12 @@ import httplib2
 
 
 def ensure_certifi_certs_installed():
-    "Give certifi's latest pem to httplib2"
+    """Give certifi's latest pem to httplib2.
+
+    This function was added to give httplib2 a more up to date collection of
+    trusted CAs. It was required once we started using flask_oauth which relies
+    on ``httplib2`` rather than ``requests``
+    """
     source = certifi.where()
     destination = join(dirname(httplib2.__file__), 'cacerts.txt')
     if not filecmp.cmp(source, destination, shallow=False):
