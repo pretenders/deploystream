ac106ebc0631e66097d4ef1af3f15fc3ef7ce562
Alex Couper <amcouper@gmail.com>
2013-04-17 00:27:02 +0100

    Add test strategy doc.

--- tests/test_providers/test_jira_provider.py
+++ tests/test_providers/test_jira_provider.py
@@ -4,10 +4,17 @@ from deploystream.providers.jira import JiraProvider
 from deploystream.providers.interfaces import (
         IPlanningProvider, is_implementation)
 
+ISSUES = []
+
 
 def test_is_planning_provider():
     assert_true(is_implementation(JiraProvider, IPlanningProvider))
 
 
 def test_jira_provider_features():
+    """
+    Patch requests to use the response in ISSUES.
+
+    Check that we get expected data out from ``get_features`` for jira.
+    """
     raise Exception("Not implemented yet")
