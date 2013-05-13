78c40277ee42a33432c7f32fb2e2c495a5916743
Alex Couper <alex.couper@glassesdirect.com>
2013-05-13 11:50:44 +0100

    Change test to import uname and password for testing purposes.

--- config/local_settings_test.py
+++ config/local_settings_test.py
@@ -1,5 +1,5 @@
 GITHUB_CONFIG = {
-    'organization': 'pretenders',
+    'organization': 'pretenders'
 }
 
 SPRINTLY_CONFIG = {
@@ -13,3 +13,14 @@ SPRINTLY_CONFIG = {
 
 GIT_CONFIG = {
 }
+
+try:
+    from non_github_settings import GITHUB_USERNAME, GITHUB_PASSWORD
+    GITHUB_CONFIG['username'] = GITHUB_USERNAME
+    GITHUB_CONFIG['password'] = GITHUB_PASSWORD
+except ImportError:
+    print ("Failed to import from non_github_settings. \n"
+        "You need GITHUB_PASSWORD and GITHUB_USERNAME defined in a module "
+        "named ``non_github_settings`` in order to run the tests."
+        )
+    raise
