7499a872ce7ba7392b73b295c3f7393a856d1bee
Alex Couper <amcouper@gmail.com>
2013-05-31 16:11:39 +0100

    Fix git fetch of test repo.

--- tests/test_providers/test_git_provider.py
+++ tests/test_providers/test_git_provider.py
@@ -15,16 +15,14 @@ def ensure_dummy_clone_available():
     if not os.path.exists(DUMMY_CODE_DIR):
         os.mkdir(DUMMY_CODE_DIR)
     folder_name = join(DUMMY_CODE_DIR, 'dummyrepo')
-
     if not exists(folder_name):
-
         os.system('git clone git://github.com/pretenders/dummyrepo.git {0}'
                   .format(folder_name))
     else:
-        print "RUNNING GIT FETCH"
         cmd = 'git --git-dir={0}/.git fetch'.format(folder_name)
-        print cmd
-        os.system(cmd)
+        ans = os.system(cmd)
+        if ans != 0:
+            raise Exception("Git fetch failed")
 
 
 @with_setup(ensure_dummy_clone_available)
@@ -53,7 +51,7 @@ def test_git_provider_feature_breakup_regex():
     Test that GitProvider breaks up feature ids into appropriate parts.
     """
     provider = GitProvider(
-                 feature_breakup_regex="(?P<project>[a-zA-Z]+)-?(?P<id>[0-9]+)")
+        feature_breakup_regex="(?P<project>[a-zA-Z]+)-?(?P<id>[0-9]+)")
     for feature, expected in [
         ('DD-334', {'id': '334', 'project':'DD'}),
         ('DD334', {'id': '334', 'project':'DD'}),
