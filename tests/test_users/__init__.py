a2abdf77cb2353db4bc0c9e6150e5b6d9350fe8a
Alex Couper <alex.couper@glassesdirect.com>
2013-06-03 21:39:02 +0000

    Remove indent.

--- tests/test_users/__init__.py
+++ tests/test_users/__init__.py
@@ -5,5 +5,5 @@ import deploystream
 
 def setup():
     if os.path.exists(deploystream.app.config['TEST_DB_LOCATION']):
-            os.remove(deploystream.app.config['TEST_DB_LOCATION'])
+        os.remove(deploystream.app.config['TEST_DB_LOCATION'])
     deploystream.db.create_all()
